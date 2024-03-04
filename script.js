67173d89f8ab150d062d794341ba547080aebd0a
Kothar Esau <tresinisa@gmail.com>
2024-03-04 20:09:21 +1300

    mission 2

---
+++ script.js
@@ -0,0 +1,283 @@
+console.log("hi");
+
+const correctBox = document.getElementById("green-box");
+const wrongBox = document.getElementById("red-box");
+
+let greenClicks = 0;
+let redClicks = 0;
+
+
+
+
+
+const a = document.getElementById('a');
+const b = document.getElementById("b");
+const c = document.getElementById("c");
+const d = document.getElementById("d");
+const e = document.getElementById("e");
+const f = document.getElementById("f");
+const g = document.getElementById("g");
+const h = document.getElementById("h");
+const i = document.getElementById("i");
+const j = document.getElementById("j");
+const k = document.getElementById("k");
+const l = document.getElementById("l");
+const m = document.getElementById("m");
+const n = document.getElementById("n");
+const o = document.getElementById("o");
+const p = document.getElementById("p");
+
+const handleGreenBox = () => {
+    style.backgroundColor = "lawngreen"
+
+    const clicks = 0;
+
+    displayScoreBoard()
+}
+
+
+a.addEventListener('click', function(){
+    a.style.backgroundColor = "lawngreen";
+});
+b.addEventListener('click', function(){
+    b.style.backgroundColor = "lawngreen";
+});
+c.addEventListener('click', function(){
+    c.style.backgroundColor = "lawngreen";
+});
+d.addEventListener('click', function(){
+    d.style.backgroundColor = "lawngreen";
+});
+e.addEventListener('click', function(){
+    e.style.backgroundColor = "lawngreen";
+});
+f.addEventListener('click', function(){
+    f.style.backgroundColor = "lawngreen";
+});
+g.addEventListener('click', function(){
+    g.style.backgroundColor = "lawngreen";
+});
+h.addEventListener('click', function(){
+    h.style.backgroundColor = "lawngreen";
+});
+i.addEventListener('click', function(){
+    i.style.backgroundColor = "lawngreen";
+});
+j.addEventListener('click', function(){
+    j.style.backgroundColor = "lawngreen";
+});
+k.addEventListener('click', function(){
+    k.style.backgroundColor = "lawngreen";
+});
+l.addEventListener('click', function(){
+    l.style.backgroundColor = "lawngreen";
+});
+m.addEventListener('click', function(){
+    m.style.backgroundColor = "lawngreen";
+});
+n.addEventListener('click', function(){
+    n.style.backgroundColor = "lawngreen";
+});
+o.addEventListener('click', function(){
+    o.style.backgroundColor = "lawngreen";
+});
+p.addEventListener('click', function(){
+    p.style.backgroundColor = "lawngreen";
+});
+
+const aa = document.getElementById("aa");
+const bb = document.getElementById("bb");
+const cc = document.getElementById("cc");
+const dd = document.getElementById("dd");
+const ee = document.getElementById("ee");
+const ff = document.getElementById("ff");
+const gg = document.getElementById("gg");
+const hh = document.getElementById("hh");
+const ii = document.getElementById("ii");
+const jj = document.getElementById("jj");
+const kk = document.getElementById("kk");
+const ll = document.getElementById("ll");
+const mm = document.getElementById("mm");
+const nn = document.getElementById("nn");
+const oo = document.getElementById("oo");
+const pp = document.getElementById("pp");
+const qq = document.getElementById("qq");
+const rr = document.getElementById("rr");
+const ss = document.getElementById("ss");
+const tt = document.getElementById("tt");
+const uu = document.getElementById("uu");
+const vv = document.getElementById("vv");
+const ww = document.getElementById("ww");
+const xx = document.getElementById("xx");
+const yy = document.getElementById("yy");
+const zz = document.getElementById("zz");
+const aaa = document.getElementById("aaa");
+const bbb = document.getElementById("bbb");
+const ccc = document.getElementById("ccc");
+const ddd = document.getElementById("ddd");
+const eee = document.getElementById("eee");
+const fff = document.getElementById("fff");
+const ggg = document.getElementById("ggg");
+const hhh = document.getElementById("hhh");
+const iii = document.getElementById("iii");
+const jjj = document.getElementById("jjj");
+const kkk = document.getElementById("kkk");
+const lll = document.getElementById("lll");
+const mmm = document.getElementById("mmm");
+const nnn = document.getElementById("nnn");
+const ooo = document.getElementById("ooo");
+const ppp = document.getElementById("ppp");
+const qqq = document.getElementById("qqq");
+const rrr = document.getElementById("rrr");
+const sss = document.getElementById("sss");
+const ttt = document.getElementById("ttt");
+const uuu = document.getElementById("uuu");
+const vvv = document.getElementById("vvv");
+
+aa.addEventListener('click', function(){
+    aa.style.backgroundColor = "red";
+});
+bb.addEventListener('click', function(){
+    bb.style.backgroundColor = "red";
+});
+cc.addEventListener('click', function(){
+    cc.style.backgroundColor = "red";
+});
+dd.addEventListener('click', function(){
+    dd.style.backgroundColor = "red";
+});
+ee.addEventListener('click', function(){
+    ee.style.backgroundColor = "red";
+});
+ff.addEventListener('click', function(){
+    ff.style.backgroundColor = "red";
+});
+gg.addEventListener('click', function(){
+    gg.style.backgroundColor = "red";
+});   
+hh.addEventListener('click', function(){
+    hh.style.backgroundColor = "red";
+});
+ii.addEventListener('click', function(){
+    ii.style.backgroundColor = "red";
+});
+jj.addEventListener('click', function(){
+    jj.style.backgroundColor = "red";
+});
+kk.addEventListener('click', function(){
+    kk.style.backgroundColor = "red";
+});
+ll.addEventListener('click', function(){
+    ll.style.backgroundColor = "red";
+});
+mm.addEventListener('click', function(){
+    mm.style.backgroundColor = "red";
+});
+nn.addEventListener('click', function(){
+    nn.style.backgroundColor = "red";
+});
+oo.addEventListener('click', function(){
+    oo.style.backgroundColor = "red";
+});
+pp.addEventListener('click', function(){
+    pp.style.backgroundColor = "red";
+});
+qq.addEventListener('click', function(){
+    qq.style.backgroundColor = "red";
+});
+rr.addEventListener('click', function(){
+    rr.style.backgroundColor = "red";
+});
+ss.addEventListener('click', function(){
+    ss.style.backgroundColor = "red";
+});
+tt.addEventListener('click', function(){
+    tt.style.backgroundColor = "red";
+});
+uu.addEventListener('click', function(){
+    uu.style.backgroundColor = "red";
+});
+vv.addEventListener('click', function(){
+    vv.style.backgroundColor = "red";
+});
+ww.addEventListener('click', function(){
+    ww.style.backgroundColor = "red";
+});
+xx.addEventListener('click', function(){
+    xx.style.backgroundColor = "red";
+});
+yy.addEventListener('click', function(){
+    yy.style.backgroundColor = "red";
+});
+zz.addEventListener('click', function(){
+    zz.style.backgroundColor = "red";
+});
+aaa.addEventListener('click', function(){
+    aaa.style.backgroundColor = "red";
+});
+bbb.addEventListener('click', function(){
+    bbb.style.backgroundColor = "red";
+});
+ccc.addEventListener('click', function(){
+    ccc.style.backgroundColor = "red";
+});
+ddd.addEventListener('click', function(){
+    ddd.style.backgroundColor = "red";
+});
+eee.addEventListener('click', function(){
+    eee.style.backgroundColor = "red";
+});
+fff.addEventListener('click', function(){
+    fff.style.backgroundColor = "red";
+});
+ggg.addEventListener('click', function(){
+    ggg.style.backgroundColor = "red";
+});
+hhh.addEventListener('click', function(){
+    hhh.style.backgroundColor = "red";
+});
+iii.addEventListener('click', function(){
+    iii.style.backgroundColor = "red";
+});
+jjj.addEventListener('click', function(){
+    jjj.style.backgroundColor = "red";
+});
+kkk.addEventListener('click', function(){
+    kkk.style.backgroundColor = "red";
+});
+lll.addEventListener('click', function(){
+    lll.style.backgroundColor = "red";
+});
+mmm.addEventListener('click', function(){
+    mmm.style.backgroundColor = "red";
+});
+nnn.addEventListener('click', function(){
+    nnn.style.backgroundColor = "red";
+});
+ooo.addEventListener('click', function(){
+    ooo.style.backgroundColor = "red";
+});
+ppp.addEventListener('click', function(){
+    ppp.style.backgroundColor = "red";
+});
+qqq.addEventListener('click', function(){
+    qqq.style.backgroundColor = "red";
+});
+rrr.addEventListener('click', function(){
+    rrr.style.backgroundColor = "red";
+});
+sss.addEventListener('click', function(){
+    sss.style.backgroundColor = "red";
+});
+ttt.addEventListener('click', function(){
+    ttt.style.backgroundColor = "red";
+});
+uuu.addEventListener('click', function(){
+    uuu.style.backgroundColor = "red";
+});
+vvv.addEventListener('click', function(){
+    vvv.style.backgroundColor = "red";
+});
+
+
+
